a571f219f8cbe1310454fdb48066b57dfbd012dc
soulike <316413310@qq.com>
2018-12-26 10:51:03 +0800

    修改数据实现

--- router.js
+++ router.js
@@ -11,46 +11,16 @@ router.get('/server/getCO2Data', async (ctx, next) =>
 
         const sheet = sheets.Sheets[sheets.SheetNames[0]];
         const sheetData = xlsx.utils.sheet_to_json(sheet);
-
-        const options = [];
-        let maxValue = 0;
         const YEAR_START = 1960;
         const YEAR_END = 2014;
 
-        for (let i = YEAR_START; i <= YEAR_END; i++)
-        {
-            const data = [];
-            for (const row of sheetData)
-            {
-                if (row[i.toString()] !== undefined)
-                {
-                    data.push({name: row['Country Name'], value: parseFloat(row[i.toString()])});
-                    if (parseFloat(row[i.toString()]) > maxValue)
-                    {
-                        maxValue = parseFloat(row[i.toString()]);
-                    }
-                }
-            }
-            options.push({
-                title: {
-                    text: `全球二氧化碳排放分布（${i} 年）`
-                },
-                visualMap: {
-                    max: maxValue,
-                },
-                series: [{
-                    data
-                }]
-            });
-            maxValue = 0;
-        }
 
         ctx.body = {
             code: 200,
             data: {
+                sheetData,
                 YEAR_START,
-                YEAR_END,
-                options
+                YEAR_END
             }
         };
     }
